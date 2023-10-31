f0d172dd4db3d39d0a10c3f24df8bfb5d48fc277
Oleg Barovskiy <oleg.sour@gmail.com>
2023-10-31 17:56:24 +0300

    test task for picasso

--- test/picasso_test/urls.py
+++ test/picasso_test/urls.py
@@ -1,14 +1,11 @@
 from django.contrib import admin
-from django.urls import path, include
-from rest_framework import routers
+from django.urls import path
 
-from files.views import files_view, upload_view, FilesView, UploadView
+from files.views import FilesView, UploadView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('files/', FilesView.as_view(), name='upload'),
     path('upload/', UploadView.as_view(), name='upload'),
-    path('visual/files_view/', files_view, name='files_view'),
-    path('visual/upload_view/', upload_view, name='upload_view'),
 ]
